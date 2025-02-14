b00950b10d066ed3d6fb0d1aa47c4dc479dfb0ae
Giangchu23 <144558129+Giangchu23@users.noreply.github.com>
2025-02-14 09:28:39 +0700

    Init project

---
+++ Assets/Script/DestroyableRock.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DestroyableRock : Rock
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
